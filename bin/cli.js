0ee56b5f228e75fc40a6b8db020ea4262805069f
Dominik Burgdörfer <db@domachine.de>
2016-10-16 16:57:08 +0200

    Add pull command
    This closes #9.

--- bin/cli.js
+++ bin/cli.js
@@ -1,39 +1,53 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const { spawn } = require('child_process')
+const { resolve } = require('path')
 const chalk = require('chalk')
-const yargs = require('yargs')
-
-const commands = require('../lib/commands')
+const minimist = require('minimist')
 
 console.log()
 console.log(chalk.bold.green('    web-design.io • commander'))
 console.log(chalk.dim.green('      - ready to take off -'))
 console.log()
 
-const argv = yargs
-  .usage('    $0 <command> [options] [args]')
-  .command('deploy', 'Deploy the current website to a cluster')
-  .command('login', 'Login to a web-design.io cluster')
-  .command('build', 'Build the current website')
-  .command('init', 'Initialize a new website')
-  .command('trigger-build', 'Trigger a build job for the website')
-  .command('push', 'Push local data to the cluster')
-  .demand(1)
-  .argv
+const argv = minimist(process.argv.slice(2))
 
-const command = commands[argv._[0]]
-if (!command) {
-  yargs.showHelp()
-  process.exit(1)
-}
-const ctx = { argv, rc: {} }
-if (fs.existsSync('.webdesigniorc.json')) {
-  ctx.rc = require(`${process.cwd()}/.webdesigniorc.json`)
-}
-const maybePromise = command(ctx, done)
-if (maybePromise && (typeof maybePromise.then) === 'function') {
-  maybePromise.then(msg => done(null, msg), err => done(err))
+const commands = new Set(['pull'])
+if (commands.has(argv._[0])) {
+  const bin = resolve(__dirname, `webdesignio-${argv._[0]}`)
+  const proc = spawn(bin, process.argv.slice(3), { stdio: 'inherit' })
+  proc.on('error', e => done(e))
+  proc.on('close', code => process.exit(code))
+} else {
+  const command = require('../lib/commands')[argv._[0]]
+  if (!command) {
+    console.log(
+`    Usage:
+
+      $ webdesignio <command> [options] [args]
+
+    Commands:
+      deploy         Deploy the current website to a cluster
+      login          Login to a web-design.io cluster
+      build          Build the current website
+      init           Initialize a new website
+      trigger-build  Trigger a build job for the website
+      push           Push local data to the cluster
+      pull           Pull the remote data to the local database
+`
+    )
+    process.exit(1)
+  } else {
+    const ctx = { argv, rc: {} }
+    if (fs.existsSync('.webdesigniorc.json')) {
+      ctx.rc = require(`${process.cwd()}/.webdesigniorc.json`)
+    }
+    const maybePromise = command(ctx, done)
+    if (maybePromise && (typeof maybePromise.then) === 'function') {
+      maybePromise.then(msg => done(null, msg), err => done(err))
+    }
+  }
 }
 
 function done (err, msg) {
